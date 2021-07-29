8602a11d09c15a8ac8b27f85dbb8a54bf6b91be9
BobCoderS9 <bobcoderss@gmail.com>
2021-07-29 14:45:31 +0800

    Fix 复制订阅链接错误BUG

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" href="/favicon.ico">
-    <title>Bob-Argon</title>
+    <title>bob-argon-v2board-vue</title>
     <link rel="stylesheet" href="https://unpkg.com/element-plus/lib/theme-chalk/index.css">
     <link href="/css/app.4644ca90.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.4f06f28f.css" rel="preload" as="style">
-    <link href="/js/app.48d31629.js" rel="preload" as="script">
+    <link href="/js/app.047cde01.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.287a91ee.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.4f06f28f.css" rel="stylesheet">
     <link href="/css/app.4644ca90.css" rel="stylesheet">
@@ -31,6 +31,6 @@
     }
 </script>
 <script src="/js/chunk-vendors.287a91ee.js"></script>
-<script src="/js/app.48d31629.js"></script>
+<script src="/js/app.047cde01.js"></script>
 </body>
 </html>
